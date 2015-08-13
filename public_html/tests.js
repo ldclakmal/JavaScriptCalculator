f07dfcc8312422c5e0d9451cb97c3209494eff33
vinujan59 <vinujan59@gmail.com>
2015-08-13 11:04:14 +0530

    Test cases completed

--- public_html/tests.js
+++ public_html/tests.js
@@ -9,3 +9,35 @@ QUnit.test("Testing Plus function", function(assert) {
 QUnit.test("Testing Plus function with failing", function(assert) {
   assert.equal(5, plus(2, 2), "2 + 2 is 5");
 });
+
+QUnit.test("Testing minus function", function(assert) {
+  assert.equal(2, minus(5, 3), "5 - 3 is 2");
+});
+
+QUnit.test("Testing minus function with failing", function(assert) {
+  assert.equal(1, minus(2, 2), "2 - 2 is 1");
+});
+
+QUnit.test("Testing multiply function", function(assert) {
+  assert.equal(15, multiply(5, 3), "5 * 3 is 15");
+});
+
+QUnit.test("Testing multiply function with failing", function(assert) {
+  assert.equal(5, multiply(2, 2), "2 * 2 is 5");
+});
+
+QUnit.test("Testing divide function", function(assert) {
+  assert.equal(2, divide(6, 3), "6 / 3 is 2");
+});
+
+QUnit.test("Testing divide function with failing", function(assert) {
+  assert.equal(2, divide(2, 2), "2 / 2 is 2");
+});
+
+QUnit.test("Testing log10 function", function(assert) {
+  assert.equal(2, log10(100), "log10(100) is 2");
+});
+
+QUnit.test("Testing log10 function with failing", function(assert) {
+  assert.equal(2, log10(1000), "log10(1000) is 2");
+});
