b3800f223df9399a2fa27e57a216cff4d98f22cd
rotiya <alwisroshan.13@cse.mrt.ac.lk>
2015-08-13 10:16:46 +0530

    First Commit

--- public_html/calculator.js
+++ public_html/calculator.js
@@ -5,3 +5,19 @@ function plus(a, b) {
 function minus(a, b) {
     return a - b;
 }
+
+function multiply(a, b){
+    return a * b;
+}
+
+function divide(a, b){
+    if(b !== 0){
+        return a / b;
+    }else{
+        return "infinite";
+    }
+}
+
+function log10(a) {
+  return Math.log(a) / Math.LN10;
+}
